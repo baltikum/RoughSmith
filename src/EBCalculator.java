8baf74f702fabb0f29f2543d9a7da1046b1da183
baltikum <mwdavidsson@outlook.com>
2021-02-25 22:43:59 +0100

    test

--- src/EBCalculator.java
+++ src/EBCalculator.java
@@ -4,5 +4,6 @@ public class EBCalculator {
     }
 
     public Double convertEBCtoSRM(Double value) { return value/1.97; }
+    
     public Double convertSRMtoEBC(Double value) { return value*1.97; }
 }
